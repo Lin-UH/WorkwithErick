229e4728a0a15bcae04c3c862214dcd954d03401
Lin-UH <60165590+Lin-UH@users.noreply.github.com>
2021-01-27 18:28:26 +0800

    retest112

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('retest1')
+    print_hi('retest21')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
